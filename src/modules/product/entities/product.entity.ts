52921c84adf364e0bdb2f70b64b744cdd64ab5cf
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-10 17:27:04 -0500

    feat(product): creation of entity

---
+++ src/modules/product/entities/product.entity.ts
@@ -0,0 +1,58 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tbl_product' })
+export class Product {
+  @PrimaryGeneratedColumn({ name: 'int_id' })
+  id: number;
+
+  @Column({ name: 'vch_handle', length: 100 })
+  handle: string;
+
+  @Column({ name: 'vch_title', length: 100 })
+  title: string;
+
+  @Column({ name: 'vch_description', length: 3000 })
+  description: string;
+
+  @Column({ name: 'int_stock' })
+  stock: number;
+
+  @Column({
+    name: 'dec_weight_in_grams',
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+  })
+  weightInGrams: number;
+
+  @Column({ name: 'vch_sku', length: 20 })
+  sku: string;
+
+  @Column({ name: 'vch_barcode', length: 20 })
+  barcode: string;
+
+  @Column({ name: 'dec_price', type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({
+    name: 'dec_compare_price',
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+  })
+  comparePrice: number;
+
+  @DeleteDateColumn({ name: 'dat_deleted_at', nullable: true })
+  deletedAt: Date | null;
+
+  @Column({ name: 'int_created_by' })
+  createdBy: number;
+
+  @Column({ name: 'int_updated_by', nullable: true })
+  updatedBy: number | null;
+}
