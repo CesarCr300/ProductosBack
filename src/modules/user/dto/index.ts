d19c1866741ef1bd634131e37309c0e36bffaec7
CesarCr300 <cc30122003@gmail.com>
2024-05-12 11:24:58 -0500

    feat(user): implementing logic to recover password

--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -3,3 +3,4 @@ export * from './filter-user.dto';
 export * from './get-link-to-reset-password-user.dto';
 export * from './update-password-user.dto';
 export * from './update-user.dto';
+export * from './recover-password-user.dto';
