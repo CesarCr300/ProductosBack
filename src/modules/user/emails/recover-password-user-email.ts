56da37cc98506cc7d253ebee73044b889f79f93e
CesarCr300 <cesarjcontreras30@gmail.com>
2024-05-12 10:56:01 -0500

    feat(user): implementing generation of token to recover-password and sending it by email to the user

---
+++ src/modules/user/emails/recover-password-user-email.ts
@@ -0,0 +1,8 @@
+export const getRecoverPasswordUserEmail = (
+  frontHost: string,
+  token: string,
+) => {
+  const content = `<p>Hola, hemos recibidio tu solicitud para recuperar tu contraseña </p><a href="${frontHost}users/recover-password?token=${token}">dale click aquí</a>`;
+
+  return { content, subject: 'Recuperar contraseña' };
+};
