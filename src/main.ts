c7548072691d1b4b341386f208644bc275e3de3f
CesarCr300 <cc30122003@gmail.com>
2024-05-14 09:33:36 -0500

    build: adding the property frontend_url to the .env to be used in the cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from 'process';
 
 import { AppModule } from './modules/app/app.module';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
-    origin: 'http://localhost:3030',
+    origin: env.FRONTEND_URL,
     methods: 'GET,POST,DELETE,PATCH, OPTIONS',
     allowedHeaders: '*',
   });
@@ -28,6 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(env.PORT);
 }
 bootstrap();
--- src/modules/user/emails/recover-password-user-email.ts
+++ src/modules/user/emails/recover-password-user-email.ts
@@ -2,7 +2,7 @@ export const getRecoverPasswordUserEmail = (
   frontHost: string,
   token: string,
 ) => {
-  const content = `<p>Hola, hemos recibidio tu solicitud para recuperar tu contraseña </p><a href="${frontHost}users/recover-password?token=${token}">dale click aquí</a>`;
+  const content = `<p>Hola, hemos recibidio tu solicitud para recuperar tu contraseña </p><a href="${frontHost}/users/recover-password?token=${token}">dale click aquí</a>`;
 
   return { content, subject: 'Recuperar contraseña' };
 };
